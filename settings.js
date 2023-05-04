52da9ffe02f5d5b8f0484bc478feed2028fd2b5c
Kike Ramírez <eramirez@barbaraiot.com>
2023-05-05 01:52:11 +0200

    Class Setting added, some input displayed on screen

--- settings.js
+++ settings.js
@@ -4,10 +4,32 @@ class Settings {
     this.enable = false;
     this.width = w;
     this.height = h;
+
+    this.settingsNameTitle = createElement("h2", "JSON Settings name");
+    this.settingsNameTitle.position(0.1 * this.width, 0.03 * this.height);
+    this.settingsNameTitle.hide();
+    
+    this.settingsNameInput = createInput(jsondata.name, "text");
+    this.settingsNameInput.position(0.1 * this.width, 0.1 * this.height);
+    this.settingsNameInput.hide();
+
+    this.backgroundColorTitle = createElement("h2", "Background Color");
+    this.backgroundColorTitle.position(0.1 * this.width, 0.15 * this.height);
+    this.backgroundColorTitle.hide();
+
     this.backgroundColorPicker = createColorPicker(jsondata.chromacolor);
-    this.backgroundColorPicker.position(0.1 * this.width, 0.1 * this.height);
-    this.backgroundColorPicker.size(0.1*this.width, AUTO);
+    this.backgroundColorPicker.position(
+      this.backgroundColorTitle.x, //+ this.backgroundColorTitle.width,
+      0.22 * this.height
+    );
+    this.backgroundColorPicker.size(0.08 * this.width, 0.05 * this.height);
     this.backgroundColorPicker.hide();
+
+    this.backgroundColorInput = createInput(jsondata.chromacolor, "text");
+    this.backgroundColorInput.position(0.1 * this.width + this.backgroundColorPicker.width, 0.22 * this.height);
+    this.backgroundColorInput.size(AUTO,  0.05 * this.height)
+    this.backgroundColorInput.hide();
+
   }
 
   resize(w, h) {
@@ -18,10 +40,17 @@ class Settings {
   setVisibility(enable) {
     this.enable = enable;
     if (enable) {
+      this.backgroundColorInput.show();
+      this.backgroundColorTitle.show();
       this.backgroundColorPicker.show();
-    }
-    else {
+      this.settingsNameInput.show();
+      this.settingsNameTitle.show();
+    } else {
+      this.backgroundColorInput.hide();
+      this.backgroundColorTitle.hide();
       this.backgroundColorPicker.hide();
+      this.settingsNameInput.hide();
+      this.settingsNameTitle.hide();
     }
   }
 
